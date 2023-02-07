af1d9106dff6a3b4df8de93d598f56e06f13ef6a
VNA818-RPI <Arcerv@rpi.edu>
2023-02-07 18:01:15 -0500

    Created Accordion Component based on provided designs

---
+++ UI.js
@@ -0,0 +1,8 @@
+/*CSS shared by many components*/
+import './UI.css';
+
+/*
+Components exported below for easy import: 
+  import { ButtonA, ButtonB, TextInput, Accordion } from './components/UI/UI';
+*/
+export { Accordion } from './accordion/accordion';
