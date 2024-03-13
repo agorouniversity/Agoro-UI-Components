b7cc42d710892ae2ae5006944fe25a0628bcc878
Gwyneth Yuen <gwyneth.yuen@gmail.com>
2024-03-13 16:46:53 -0400

    Add header test page and add props

--- docs/src/pages/header.jsx
+++ docs/src/pages/header.jsx
@@ -1,19 +1,19 @@
 import React from 'react';
-import { Header, ButtonA } from '@agorouniversity/agoro-ui-components';
+import { Header, Button } from '@agorouniversity/agoro-ui-components';
 
 export default function HeaderPage() {
     return (
         <>
           <Header>
-            <Header.Buttons dark={true} color='#32a852' size='large'>
-              <Button>Home</Button>
-              <Button>Options</Button>
-              <Button>Logout</Button>
+            <Header.Buttons dark={false} color='#32a852' size='small'>
+              <Button link="/" size='small' color='Danger'>Home</Button>
+              <Header.DropDown>
+                <Button>System</Button>
+                <Button>Light</Button>
+                <Button>Dark</Button>
+              </Header.DropDown>
+              <Button link="/logout" size='small'>Logout</Button>
             </Header.Buttons>
-            <Header.DropDown>
-            {/* <Login /> */}
-              <Button>Option 1</Button>
-            </Header.DropDown>
           </Header>
         </>
         )
--- src/header/header.jsx
+++ src/header/header.jsx
@@ -3,7 +3,8 @@ import Logo from '../images/logo.png';
 import LogoDark from '../images/logoDark.png';
 import './header.css';
 import { Link } from 'react-router-dom';
-import { DropDown } from '../UI';
+import { Button, DropDown } from '../UI';
+import {Theme} from "../theme/Theme";
 
 const HeaderButtons = (props) => {
   return(
@@ -19,7 +20,7 @@ const HeaderButtons = (props) => {
             className={`logo img${props.size ? ' ' + props.size : ''}`}
             style={{backgroundImage: `url(${props.dark ? LogoDark : Logo})`}}
           ></div>
-          <h1 className={`h1${props.size ? ' ' + props.size : ''}`}>Agoro</h1>
+          <h1 className={`h1${props.size ? ' ' + props.size : ''} ${props.dark ? 'dark' : 'light'}`}>Agoro</h1>
         </Link>
         <div
           className='rightCol'
@@ -37,32 +38,12 @@ const HeaderDropDown = (props) => {
     >
       <DropDown>
         <DropDown.Button>
-          <button>Options</button>
-          {/* {props.children} */}
+          <Button size="small">Options</Button>
         </DropDown.Button>
-        {/* <DropDown.Menu>
+        <DropDown.Menu dark={false}>
           <span>Theme</span>
-            <ul>
-              <li
-                className={darkMode.mode === 'system' ? 'selected' : null}
-                onClick={() => changeTheme('system')}
-              >
-                <button>System</button>
-              </li>
-              <li
-                className={darkMode.mode === 'light' ? 'selected' : null}
-                onClick={() => changeTheme('light')}
-              >
-                <button>Light</button>
-              </li>
-              <li
-                className={darkMode.mode === 'dark' ? 'selected' : null}
-                onClick={() => changeTheme('dark')}
-              >
-                <button>Dark</button>
-              </li>
-            </ul>
-        </DropDown.Menu> */}
+          {props.children}
+        </DropDown.Menu>
       </DropDown>
     </div>
   )
@@ -87,15 +68,17 @@ export function Header(props) {
   }, [])
 
   return(
-    <div
-      id={props.id}
-      className={`${props.dropDownOpen ? 'open' : ''} headerBg${props.className ? ' ' + props.className : ''}`}
-    >
+    <Theme>
       <div
-        className={`${props.dropDownOpen ? 'open' : ''} headerContainer${props.className ? ' ' + props.className : ''}`}
+        id={props.id}
+        className={`${props.dropDownOpen ? 'open' : ''} headerBg${props.className ? ' ' + props.className : ''}`}
       >
-        {props.children}
+        <div
+          className={`${props.dropDownOpen ? 'open' : ''} headerContainer${props.className ? ' ' + props.className : ''}`}
+        >
+          {props.children}
+        </div>
       </div>
-    </div>
+    </Theme>
   )
 }
